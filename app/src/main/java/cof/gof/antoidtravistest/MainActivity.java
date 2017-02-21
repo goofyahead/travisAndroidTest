8c3ea226b4a5b04250c701bd66f113af2af3f745
Alex Vidal <goofyahead@gmail.com>
2017-02-21 16:04:06 +0400

    initial commit

---
+++ app/src/main/java/cof/gof/antoidtravistest/MainActivity.java
@@ -0,0 +1,13 @@
+package cof.gof.antoidtravistest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
